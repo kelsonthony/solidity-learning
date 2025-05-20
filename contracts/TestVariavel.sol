0d67005f3038b0634f556b279c5c4f85394dbfc6
kelsonthony <k.thony@gmail.com>
2025-05-20 07:42:10 -0300

    first commit

--- contracts/TestVariavel.sol
+++ contracts/TestVariavel.sol
@@ -3,7 +3,13 @@ pragma solidity ^0.8.7;
 
 contract TesteVariaveis {
 
-    string public texto1 = "Ola mundo";
+    string texto1 = "Ola mundo";
+    
     string public texto2;
+
+    string internal  texto3 = "Ola mundo 2";
+    string private texto4;
+
+    //[TIPO] [VISIBILIDADE] [NOME]
     
 }
